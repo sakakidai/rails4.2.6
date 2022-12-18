254bb5968a3c88a33d506f4d75feda30c9ecc008
sakakidai <sd.lybd1016@gmail.com>
2022-12-18 10:22:11 +0900

    capistranoの設定変更

--- Gemfile
+++ Gemfile
@@ -57,5 +57,4 @@ group :development do
 end
 
 gem 'unicorn'
-gem 'unicorn-rails'
 gem 'dotenv-rails'
--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,6 @@ set :deploy_to, '/var/www/rails4.2.6'
 
 # レポジトリの設定
 set :repo_url, "git@github.com:sakakidai/rails4.2.6.git"
-set :scm, :git
 
 # sharedディレクトリに入れるファイルを指定
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
@@ -22,9 +21,10 @@ set :rbenv_ruby, '2.6.6'
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 
-# unicornを再起動するコマンド
-after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
+  after :publishing, :restart
+
+  # unicornを再起動するコマンド
   task :restart do
     invoke 'unicorn:restart'
   end
